3ce6996da4c48746011b9de1338e380fd9d9a3a5
長谷川喜洋 <hasegawayoshihiro@hasegawayoshihiro-no-MacBook-Pro.local>
2018-01-27 15:36:32 +0900

    add serch bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { EventPage} from "../pages/event/event";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    EventPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +19,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    EventPage
   ],
   providers: [
     StatusBar,
